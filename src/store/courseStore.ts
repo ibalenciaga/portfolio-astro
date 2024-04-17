f33f4dd0ba43a0b1009740060e7abeec099fb78f
ibalenciaga <ibalenciaga@gmail.com>
2024-04-17 12:05:24 +0200

    create portfolio

---
+++ src/store/courseStore.ts
@@ -0,0 +1,72 @@
+const COURSES = [
+    {
+        date: "2024",
+        title: "PHP8",
+    },
+    {
+        date: "2024",
+        title: "CQRS",
+    },
+    {
+        date: "2024",
+        title: "Arquitectura hexagonal",
+    },
+    {
+        date: "2023",
+        title: "Symfony 7",
+    },
+    {
+        date: "2022",
+        title: "Arquitectura Software Moderna: DDD, Eventos, Microservicios"
+    },
+    {
+        date: "2022",
+        title: "Principios SOLID con ejemplos"
+    },
+    {
+        date: "2022",
+        title: "Principios SOLID y clean code",
+    },
+    {
+        date: "2022",
+        title: "PHP, SQL, POO, MVC, Laravel, Symfony, Wordpress",
+    },
+    {
+        date: "2021",
+        title: "Prestashop",
+    },
+    {
+        date: "2021",
+        title: "Lógica de programación",
+    },
+    {
+        date: "2021",
+        title: "Creación de servidores locales",
+    },
+    {
+        date: "2020",
+        title: "Symfony 5",
+    },
+    {
+        date: "2020",
+        title: "Docker"
+    },
+    {
+        date: "2020",
+        title: "GIT",
+    },
+    {
+        date: "2019",
+        title: "Wordpress",
+    },
+    {
+        date: "2019",
+        title: "Introducción analítica web"
+    },
+    {
+        date: "2019",
+        title: "Desarrollo de Aplicaciones Web",
+    }
+]
+
+export {COURSES};
---
+++ src/store/experienceStore.ts
@@ -0,0 +1,44 @@
+const EXPERIENCE = [
+    {
+        date: "2024",
+        title: "Peña Hípica Donostiarra",
+        description: "Crear una nueva plataforma para llevar la gestión de la sociedad gastronomica. Para el backend se ha creado una API REST con el framwork Symfony y para el frontend se ha utilizado el framwork VueJs.",
+    },
+    {
+        date: "Septiembre 2022 - Diciembre 2023",
+        title: "Bibagu",
+        description: "Crear nuevas funcionalidades a la aplicación web de la empresa utilizando Codeigniter y Vue.js. Crear nuevos endpoint para la API con php y después hacer las llamadas en el front con javascript.",
+    },
+    {
+        date: "Diciembre 2021 - Marzo 2022",
+        title: "OARO",
+        description: "Crear nuevas páginas para las webs de diferentes clientes utilizando Symfony, twig, css3 y javcascript.",
+    },
+    {
+        date: "2020",
+        title: "Peña Hípica Donostiarra",
+        description: "Crear la página web de la sociedad gastronómica con php utilizando el framework Symfony.",
+    },
+    {
+        date: "2019",
+        title: "Ilargi Berdea",
+        description: "Crear la página web de la asociación con wordpress.",
+    },
+    {
+        date: "Junio 2019 - Marzo 2020",
+        title: "Lombok",
+        description: "Crear nuevas páginas web para diferentes clientes utilizando Wordpress, css3, javascript, jquery, php y mysql.",
+    },
+    {
+        date: "2018",
+        title: "Iparki",
+        description: "Crear la página web de la empresa con wordpress.",
+    },
+    {
+        date: "Octubre 2017 - Diciembre 2018",
+        title: "FeelFree Rentals",
+        description: "Crear nuevas funcionalidades a la aplicación web interna de la empresa con el framewrok Joomla. Crear nuevas páginas y funcionalidades a la web de la empresa utilizando el framwork Symfony, css3, javascript, jquery y ajax.",
+    }
+]
+
+export { EXPERIENCE } ;
